541c4d10e79fd516b6768705b7372a9b7b1c80ed
RosaPos <rposadashernndez@byupathway.edu>
2026-01-07 16:59:41 -0600

    Made adjustments to the code

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -2,6 +2,7 @@ using System;
 
 class Program
 {
+    // Entry point of the program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
