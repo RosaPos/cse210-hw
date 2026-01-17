169d2ad097897fee8d28b0387bb42065a1cea17b
RosaPos <rposadashernndez@byupathway.edu>
2026-01-17 15:39:44 -0600

    exercise2

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,12 +5,12 @@ class Program
     // Entry point of the program
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name? ");
+        Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
-        Console.WriteLine("What is your last name? ");
+        Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}. ");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int gradePercentage = int.Parse(answer);
+        string letterGrade = "";
+
+        if (gradePercentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letterGrade = "D";
+        }
+
+        else
+        {
+            letterGrade = "F";
+
+        }
+        Console.WriteLine($"Your letter grade is: {letterGrade}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the course. Better luck next time!");
+        }
     }
 }
