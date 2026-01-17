a8f311b2b846d2740302d44e2efd66630b8fb350
RosaPos <rposadashernndez@byupathway.edu>
2026-01-17 09:01:49 -0600

    W01 exercise1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,6 +5,12 @@ class Program
     // Entry point of the program
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}. ");
     }
 }
