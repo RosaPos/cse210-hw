9783d720fc448bd2bc0c6f3135bf0c094e3d9235
RosaPos <rposadashernndez@byupathway.edu>
2026-01-22 14:46:09 -0600

    W02 Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // Job 1
+        Job job1 = new Job();
+        job1._jobTitle = "Software Developer";
+        job1._company = "Tech Solutions Inc.";
+        job1._startYear = 2018;
+        job1._endYear = 2022;
+
+        Console.WriteLine(job1._company);
+
+        // Job 2
+        Job job2 = new Job();
+        job2._jobTitle = "Senior Developer";
+        job2._company = "Innovatech Corp.";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+
+        Console.WriteLine(job2._company);
+
+        Resume myResume = new Resume();
+        myResume._name = "Alex Johnson";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        Console.WriteLine(myResume._jobs[0]._jobTitle);
+
     }
 }
