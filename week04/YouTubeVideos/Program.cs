1620bb9c6e1b0ff1239e493f68b0d4b3d7350c00
RosaPos <rposadashernndez@byupathway.edu>
2026-02-07 13:11:26 -0600

    comments

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -10,8 +10,20 @@ class Program
         v1.AddComment(new Comment("Jose", "I liked it so much."));
         v1.AddComment(new Comment("Maria", "Thanks for sharing!"));
 
+        Video v2 = new Video("Encapsulation Basics", "Rosa Posadas", 180);
+        v2.AddComment(new Comment("Ana", "Very clear explanation."));
+        v2.AddComment(new Comment("Luis", "Now I understand private fields."));
+        v2.AddComment(new Comment("Karla", "Great examples!"));
+
+        Video v3 = new Video("Lists in C#", "Rosa Posadas", 240);
+        v3.AddComment(new Comment("Pedro", "Helpful practice."));
+        v3.AddComment(new Comment("Sofia", "Nice pace."));
+        v3.AddComment(new Comment("Dani", "Good tips!"));
+
         List<Video> videos = new List<Video>();
         videos.Add(v1);
+        videos.Add(v2);
+        videos.Add(v3);
 
         foreach (Video v in videos)
         {
